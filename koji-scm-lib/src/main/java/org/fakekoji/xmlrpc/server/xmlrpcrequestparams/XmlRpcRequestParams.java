3cbbcb987723ed40f5792bb198c2459eab712f08
oklinov <oklinovs@redhat.com>
2019-04-05 11:28:12 +0200

    Add javadoc to XmlRpx interfaces

--- koji-scm-lib/src/main/java/org/fakekoji/xmlrpc/server/xmlrpcrequestparams/XmlRpcRequestParams.java
+++ koji-scm-lib/src/main/java/org/fakekoji/xmlrpc/server/xmlrpcrequestparams/XmlRpcRequestParams.java
@@ -4,7 +4,22 @@ import java.io.Serializable;
 
 public interface XmlRpcRequestParams extends Serializable {
 
+    /**
+     * This method converts POJO to a map of objects with additional parameters based on XML-RPC method it is used for.
+     * This method is used by XML-RPC client for sending XML-RPC requests.
+     *
+     * @see ListRPMs
+     * @see GetPackageId
+     *
+     * @return map of objects as Object
+     */
     Object toObject();
+
+    /**
+     *  This method returns name of the XML-RPC method that this class represents parameters of.
+     *
+     * @return XML-RPC method name
+     */
     String getMethodName();
 
 }
--- koji-scm-lib/src/main/java/org/fakekoji/xmlrpc/server/xmlrpcresponse/XmlRpcResponse.java
+++ koji-scm-lib/src/main/java/org/fakekoji/xmlrpc/server/xmlrpcresponse/XmlRpcResponse.java
@@ -2,6 +2,26 @@ package org.fakekoji.xmlrpc.server.xmlrpcresponse;
 
 public interface XmlRpcResponse <T> {
 
+
+    /**
+     * This method converts POJO to maps of objects or a simple object based on xml-rpc method it is used for. This
+     * method is used in fake-koji xml-rpc api to send responses.
+     *
+     * @see RPMList
+     * @see PackageId
+     *
+     * @return map of objects as Object
+     */
     Object toObject();
+
+    /**
+     * This method converts a simple object or map of objects to POJO. It is reverse toObject method. This method is
+     * used in xml-rpc client to receive responses
+     *
+     * @see RPMList
+     * @see PackageId
+     *
+     * @return instance of requested type
+     */
     T getValue();
 }
