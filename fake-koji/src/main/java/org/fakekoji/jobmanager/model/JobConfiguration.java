ba31ed59dfd5bcb9a5f175a25f8bac0d28ca2d9a
oklinov <oklinovs@redhat.com>
2019-08-18 13:49:25 +0200

    Implement equals and hashCode in JobConfiguration

--- fake-koji/src/main/java/org/fakekoji/jobmanager/model/JobConfiguration.java
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/model/JobConfiguration.java
@@ -2,6 +2,7 @@ package org.fakekoji.jobmanager.model;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
 public class JobConfiguration {
 
@@ -17,5 +18,18 @@ public class JobConfiguration {
 
     public Map<String, PlatformConfig> getPlatforms() {
         return platforms;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobConfiguration)) return false;
+        JobConfiguration that = (JobConfiguration) o;
+        return Objects.equals(platforms, that.platforms);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(platforms);
     }
 }
