e77a87c44fb30f341b4205ed60798e3c2ea6d08e
oklinov <oklinovs@redhat.com>
2019-08-20 08:54:48 +0200

    Create ConfigManager so all config storage are accessible from one place

---
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/ConfigManager.java
@@ -0,0 +1,80 @@
+package org.fakekoji.jobmanager;
+
+import org.fakekoji.model.BuildProvider;
+import org.fakekoji.model.Platform;
+import org.fakekoji.model.Product;
+import org.fakekoji.jobmanager.model.Project;
+import org.fakekoji.model.Task;
+import org.fakekoji.model.TaskVariantCategory;
+import org.fakekoji.storage.DirectoryJsonStorage;
+import org.fakekoji.storage.Storage;
+
+import java.nio.file.Paths;
+
+public class ConfigManager {
+
+    private final static String BUILD_PROVIDERS = "buildProviders";
+    private final static String PRODUCTS = "products";
+    private final static String TASK_VARIANT_CATEGORIES = "taskVariantCategories";
+    private final static String PLATFORMS = "platforms";
+    private final static String PROJECTS = "projects";
+    private final static String TASKS = "tasks";
+
+    private final Storage<BuildProvider> buildProviderStorage;
+    private final Storage<Product> productStorage;
+    private final Storage<TaskVariantCategory> taskVariantCategoryStorage;
+    private final Storage<Platform> platformStorage;
+    private final Storage<Project> projectStorage;
+    private final Storage<Task> taskStorage;
+
+    private ConfigManager(
+            Storage<BuildProvider> buildProviderStorage,
+            Storage<Product> productStorage,
+            Storage<TaskVariantCategory> taskVariantCategoryStorage,
+            Storage<Platform> platformStorage,
+            Storage<Project> projectStorage,
+            Storage<Task> taskStorage
+    ) {
+        this.buildProviderStorage = buildProviderStorage;
+        this.productStorage = productStorage;
+        this.taskVariantCategoryStorage = taskVariantCategoryStorage;
+        this.platformStorage = platformStorage;
+        this.projectStorage = projectStorage;
+        this.taskStorage = taskStorage;
+    }
+
+    public static ConfigManager create(String storagePath) {
+        return new ConfigManager(
+                new DirectoryJsonStorage<>(Paths.get(storagePath, BUILD_PROVIDERS).toFile()),
+                new DirectoryJsonStorage<>(Paths.get(storagePath, PRODUCTS).toFile()),
+                new DirectoryJsonStorage<>(Paths.get(storagePath, TASK_VARIANT_CATEGORIES).toFile()),
+                new DirectoryJsonStorage<>(Paths.get(storagePath, PLATFORMS).toFile()),
+                new DirectoryJsonStorage<>(Paths.get(storagePath, PROJECTS).toFile()),
+                new DirectoryJsonStorage<>(Paths.get(storagePath, TASKS).toFile())
+        );
+    }
+
+    public Storage<BuildProvider> getBuildProviderStorage() {
+        return buildProviderStorage;
+    }
+
+    public Storage<Product> getProductStorage() {
+        return productStorage;
+    }
+
+    public Storage<TaskVariantCategory> getTaskVariantCategoryStorage() {
+        return taskVariantCategoryStorage;
+    }
+
+    public Storage<Platform> getPlatformStorage() {
+        return platformStorage;
+    }
+
+    public Storage<Project> getProjectStorage() {
+        return projectStorage;
+    }
+
+    public Storage<Task> getTaskStorage() {
+        return taskStorage;
+    }
+}
