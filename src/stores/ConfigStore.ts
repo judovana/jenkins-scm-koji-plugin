020b3b59aa81cabacc6be5e129c47a21c5f8dce9
oklinov <oklinovs@redhat.com>
2019-09-24 17:05:36 +0200

    Delete ProjectStore and move all data to ConfigStore

--- src/stores/ConfigStore.ts
+++ src/stores/ConfigStore.ts
@@ -1,6 +1,6 @@
-import { observable, runInAction } from "mobx";
+import { observable, runInAction, action } from "mobx";
 
-import { Platform, Product, TaskVariant, Task } from "./model";
+import { Platform, Product, TaskVariant, Task, JDKProject, Item } from "./model";
 
 export const CONFIG_STORE = "configStore";
 
@@ -18,11 +18,54 @@ export class ConfigStore {
     @observable
     private _tasks: Map<string, Task>;
 
+    @observable
+    private _jdkProjects: Map<string, JDKProject>;
+
+    @observable
+    private _selectedGroupId: string | undefined = "tasks";
+
+    @observable _selectedConfig: Item | undefined;
+
     constructor() {
         this._platforms = new Map<string, Platform>();
         this._products = new Map<string, Product>();
         this._taskVariants = new Map<string, TaskVariant>();
         this._tasks = new Map<string, Task>();
+        this._jdkProjects = new Map<string, JDKProject>();
+    }
+
+    @action
+    selectGroup(id: string) {
+        this._selectedGroupId = id;
+        this._selectedConfig = undefined;
+    }
+
+    @action
+    selectConfig(config: Item) {
+        this._selectedConfig = config;
+    }
+
+    get selectedGroupId(): string | undefined {
+        return this._selectedGroupId;
+    }
+
+    get selectedGroup(): Item[] {
+        switch (this._selectedGroupId) {
+            case "platforms": return Array.from(this._platforms.values());
+            case "products": return Array.from(this._products.values());
+            case "taskVariants": return Array.from(this._taskVariants.values());
+            case "tasks": return Array.from(this._tasks.values());
+            case "jdkProjects": return Array.from(this._jdkProjects.values());
+            default: return [];
+        }
+    }
+
+    get configGroups(): Item[] {
+        return [{ id: "platforms" }, { id: "products" }, { id: "taskVariants" }, { id: "tasks" }, { id: "jdkProjects" }];
+    }
+
+    get selectedConfig(): Item | undefined {
+        return this._selectedConfig;
     }
 
     async fetchPlatforms() {
@@ -40,7 +83,10 @@ export class ConfigStore {
     async fetchTasks() {
         const response = await fetch("http://localhost:8081/tasks");
         const tasks: Task[] = await response.json();
-        runInAction(() => tasks.forEach(task => this._tasks.set(task.id, task)));
+        runInAction(() => tasks.forEach(task => {
+            this._tasks.set(task.id, task);
+            this._selectedConfig = this._tasks.get("tck");
+        }));
     }
 
     async fetchTaskVariants() {
@@ -49,20 +95,52 @@ export class ConfigStore {
         runInAction(() => taskVariants.forEach(taskVariant => this._taskVariants.set(taskVariant.id, taskVariant)));
     }
 
-    get platforms(): Map<string, Platform> {
-        return this._platforms;
+    async fetchJDKProjects() {
+        const response = await fetch("http://localhost:8081/jdkProjects");
+        const projects: JDKProject[] = await response.json();
+        runInAction(() => {
+            projects.forEach(project => this._jdkProjects.set(project.id, project));
+        });
     }
 
-    get products(): Map<string, Product> {
-        return this._products;
+    get platforms(): Platform[] {
+        return Array.from(this._platforms.values());
     }
 
-    get taskVariants(): Map<string, TaskVariant> {
-        return this._taskVariants;
+    getPlatform(id: string): Platform | undefined {
+        return this._platforms.get(id);
     }
 
-    get tasks(): Map<string, Task> {
-        return this._tasks;
+    get products(): Product[] {
+        return Array.from(this._products.values());
+    }
+
+    getProduct(id: string): Product | undefined {
+        return this._products.get(id);
+    }
+
+    get taskVariants(): TaskVariant[] {
+        return Array.from(this._taskVariants.values());
+    }
+
+    getTaskVariant(id: string): TaskVariant | undefined {
+        return this._taskVariants.get(id);
+    }
+
+    get tasks(): Task[] {
+        return Array.from(this._tasks.values());
+    }
+
+    getTask(id: string): Task | undefined {
+        return this._tasks.get(id);
+    }
+
+    get jdkProjects(): JDKProject[] {
+        return Array.from(this._jdkProjects.values());
+    }
+
+    getJDKProject(id: string): JDKProject | undefined {
+        return this._jdkProjects.get(id);
     }
 }
 
