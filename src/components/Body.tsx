6ba13cc4e076a2e426d180a5b8ce7a90d4c6cf47
oklinov <oklinovs@redhat.com>
2019-09-25 16:34:12 +0200

    Update Body component

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,60 +1,40 @@
 import React from "react";
-
 import { inject, observer } from "mobx-react";
-import { ProjectStore, PROJECT_STORE } from "../stores/ProjectStore";
-import ProjectForm from "./ProjectForm";
+
+import ConfigForm from "./ConfigForm";
 import { CONFIG_STORE, ConfigStore } from "../stores/ConfigStore";
 
 interface Props {
-    projectStore?: ProjectStore;
     configStore?: ConfigStore;
 }
 
 class Body extends React.PureComponent<Props> {
 
     componentDidMount() {
-        const { configStore, projectStore } = this.props;
-
-        if (configStore) {
-            configStore.fetchPlatforms();
-            configStore.fetchProducts();
-            configStore.fetchTasks();
-            configStore.fetchTaskVariants();
-        }
-        if (projectStore) {
-            projectStore.fetchJDKProjects();
-        }
+        const configStore = this.props.configStore!;
+        configStore.fetchPlatforms();
+        configStore.fetchProducts();
+        configStore.fetchTasks();
+        configStore.fetchTaskVariants();
+        configStore.fetchJDKProjects();
     }
 
     render() {
-        const { projectStore } = this.props;
-
-        if (!projectStore) {
-            return <div>no store</div>;
-        }
-        const projectCategory = projectStore.selectedProjectCategory;
-        if (!projectCategory) {
-            return <div></div>
-        }
-        const projects = projectStore.projects;
-        const project = projectStore.selectedProject;
+        const configStore = this.props.configStore!;
+        const { selectedGroup, selectedConfig, selectedGroupId } = configStore;
         return (
             <div className="Body">
                 {
-                    projectCategory &&
                     <div className="List">
-                        <div className="ListHeader">
-                            {projectCategory.label}
-                        </div>
                         <div>
                             {
 
-                                Array.from(projects.values()).map(project =>
+                                selectedGroup.map(config =>
                                     <div
                                         className="ListItem"
-                                        onClick={() => projectStore.selectedProjectId = project.id}
-                                        key={project.id}>
-                                        {project.id}
+                                        onClick={() => configStore.selectConfig(config)}
+                                        key={config.id}>
+                                        {config.id}
                                     </div>
                                 )
                             }
@@ -62,14 +42,14 @@ class Body extends React.PureComponent<Props> {
                     </div>
                 }
                 {
-                    project &&
-                    <div>
-                        <ProjectForm project={project} />
-                    </div>
+                    selectedGroupId && selectedConfig && <ConfigForm
+                        group={selectedGroupId}
+                        config={selectedConfig}>
+                    </ConfigForm>
                 }
             </div>
         )
     }
 }
 
-export default inject(PROJECT_STORE, CONFIG_STORE)(observer(Body));
+export default inject(CONFIG_STORE)(observer(Body));
