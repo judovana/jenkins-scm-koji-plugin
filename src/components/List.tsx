84bbb041151d1e8897b098c2c9ffe13330b77940
oklinov <oklinovs@redhat.com>
2019-10-08 16:07:09 +0200

    Add option to delete an item in List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { inject, observer } from "mobx-react"
 import { CONFIG_STORE, ConfigStore } from "../stores/ConfigStore";
 import Button from "./Button";
+import { Item } from "../stores/model";
 
 interface Props {
     configStore?: ConfigStore
@@ -12,6 +13,11 @@ class List extends React.PureComponent<Props> {
     onCreate = () => {
         const configStore = this.props.configStore!
         configStore.selectNewConfig(configStore.selectedGroupId!)
+    }
+
+    onDelete = (config: Item) => {
+        const configStore = this.props.configStore!
+        configStore.deleteConfig(config.id)
     }
 
     render() {
@@ -31,6 +37,9 @@ class List extends React.PureComponent<Props> {
                                 key={config.id}
                                 style={{marginBottom: index < selectedGroup.length ? 2 : 0}}>
                                 {config.id}
+                                <Button
+                                    color="red"
+                                    onClick={() => this.onDelete(config)}>delete</Button>
                             </div>
                         )
                     }
