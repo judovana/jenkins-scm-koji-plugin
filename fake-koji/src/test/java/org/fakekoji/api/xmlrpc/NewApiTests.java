f4a005adc9d934463ff628c1147635e3adced695
oklinov <oklinovs@redhat.com>
2019-10-27 16:42:20 +0100

    Add tests for new api, on bothjenkins plugin and fake-koji

--- fake-koji/src/test/java/org/fakekoji/api/xmlrpc/NewApiTests.java
+++ fake-koji/src/test/java/org/fakekoji/api/xmlrpc/NewApiTests.java
@@ -1,219 +1,204 @@
 package org.fakekoji.api.xmlrpc;
 
 import hudson.plugins.scm.koji.model.Build;
+import hudson.plugins.scm.koji.model.RPM;
+import org.fakekoji.DataGenerator;
 import org.fakekoji.core.AccessibleSettings;
-import org.fakekoji.core.FakeBuild;
 import org.fakekoji.core.FakeKojiDB;
-import org.fakekoji.xmlrpc.server.xmlrpcrequestparams.GetBuildDetail;
 import org.fakekoji.xmlrpc.server.xmlrpcrequestparams.GetBuildList;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static org.fakekoji.DataGenerator.SUFFIX;
 
 public class NewApiTests {
 
-    private static AccessibleSettings as;
+    @ClassRule
+    public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
+
     private static FakeKojiDB kojiDB;
-    private static File dbRoot;
-    private static final String[] archEnum = new String[]{"src", "el7_x86_64", "el6_i686", "win10", "win2016s", "win2012_x86_64", "el8_ppc64le", "el7_aarch64"};
 
     @BeforeClass
-    public static void prepareDb() throws IOException {
-        dbRoot = File.createTempFile("fakeKoji", "dbRoot");
-        dbRoot.delete();
-        dbRoot.mkdir();
-        File builds = new File(dbRoot, "builds");
-        builds.mkdir();
-        File repos = new File(dbRoot, "repos");
-        repos.mkdir();
-        String[] projects = new String[]{"java-1.8.0-openjdk-shenandoah", "java-1.8.0-openjdk", "java-X-openjdk-shenandoah", "java-12-openjdk-updates", "java-1.7.0-openjdk-forest", "java-1.8.0-openjdk-aarch64-shenandoah", "java-11-openjdk-updates", "java-12-openjdk", "java-1.7.0-openjdk-forest-26", "java-1.8.0-openjdk-dev", "java-1.7.0-openjdk", "java-11-openjdk-shenandoah", "java-9-openjdk", "java-1.8.0-openjdk-aarch64", "java-10-openjdk", "java-9-openjdk-shenandoah", "java-X-openjdk", "java-11-openjdk", "thermostat-ng", "java-9-openjdk-updates", "java-9-openjdk-dev", "java-10-openjdk-updates"};
-        for (String project : projects) {
-            File p = new File(repos, project);
-            p.mkdir();
-            Files.write(new File(p, "something").toPath(), Arrays.asList(new String[]{"nothing", project, p.getAbsolutePath()}), StandardCharsets.UTF_8);
-            //currenlty mandatory to enumerate arches
-            //TODO, fix, once configs will be in place
-            //the arches_expected in repos should go away
-            Files.write(new File(p, FakeBuild.archesConfigFileName).toPath(), Arrays.asList(new String[]{String.join(" ", archEnum)}), StandardCharsets.UTF_8);
+    public static void setup() throws IOException {
+        final File builds = temporaryFolder.newFolder("builds");
+        final File repos = temporaryFolder.newFolder("repos");
+        final File configs = temporaryFolder.newFolder("configs");
 
+        DataGenerator.initConfigsRoot(configs.getAbsoluteFile());
+        DataGenerator.initBuildsRoot(builds);
+
+        kojiDB = new FakeKojiDB(new AccessibleSettings(
+                builds,
+                repos,
+                configs,
+                null,
+                null,
+                null,
+                9848,
+                9849,
+                9822,
+                8080,
+                0
+        ));
+    }
+
+    @Test
+    public void getSourcesOfBuilt() {
+        final int expectedNumberOfBuilds = 0;
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=release buildPlatform=f29.x86_64",
+                "src",
+                false
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
+    }
+
+    @Test
+    public void getSourcesOfPartiallyNotBuilt() {
+        final int expectedNumberOfBuilds = 2;
+        final Set<String> expectedArchives = new HashSet<>(Arrays.asList(
+                "java-1.8.0-openjdk-version2-release1.uName.src" + SUFFIX,
+                "java-1.8.0-openjdk-version2-release2.uName.src" + SUFFIX
+
+        ));
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=fastdebug buildPlatform=f29.x86_64",
+                "src",
+                false
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
+        for (final Build build : buildList) {
+            Assert.assertEquals(1, build.getRpms().size());
+            final RPM rpm = build.getRpms().get(0);
+            Assert.assertTrue(expectedArchives.contains(rpm.getFilename(SUFFIX)));
         }
-        String[] products = new String[]{"java-openjdk", "java-1.8.0-openjdk", "java-12-openjdk", "java-1.7.0-openjdk", "java-9-openjdk", "java-10-openjdk", "openjdk8-win", "java-X-openjdk", "java-11-openjdk"};
-        for (String product : products) {
-            File p = new File(builds, product);
-            p.mkdir();
-            for (String v : new String[]{"v1", "v2"}) {
-                File d = new File(p, v);
-                d.mkdir();
-                List<String> releases = new ArrayList<>();
-                for (String project : projects) {
-                    if (project.contains(product)) {
-                        String sb = project.replaceAll(product + "-*", "").replaceAll("-", ".");
-                        if (!sb.isEmpty()) {
-                            sb = "." + sb;
-                        }
-                        releases.add("r1" + sb);
-                        releases.add("r2" + sb);
-                        releases.add("r1" + sb + ".hotspot");
-                        releases.add("r2" + sb + ".hotspot");
-                        releases.add("r1" + sb + ".zero");
-                        releases.add("r2" + sb + ".zero");
-                        releases.add("r1" + sb + ".openj9");
-                        releases.add("r2" + sb + ".openj9");
-                        releases.add("r1" + sb + ".release");
-                        releases.add("r2" + sb + ".release");
-                        releases.add("r1" + sb + ".slowdebug");
-                        releases.add("r2" + sb + ".slowdebug");
-                        releases.add("r1" + sb + ".fastdebug");
-                        releases.add("r2" + sb + ".fastdebug");
-                        releases.add("r1" + sb + ".release.hotspot");
-                        releases.add("r2" + sb + ".release.hotspot");
-                        releases.add("r1" + sb + ".slowdebug.hotspot");
-                        releases.add("r2" + sb + ".slowdebug.hotspot");
-                        releases.add("r1" + sb + ".fastdebug.hotspot");
-                        releases.add("r2" + sb + ".fastdebug.hotspot");
-                        releases.add("r1" + sb + ".release.zero");
-                        releases.add("r2" + sb + ".release.zero");
-                        releases.add("r1" + sb + ".slowdebug.zero");
-                        releases.add("r2" + sb + ".slowdebug.zero");
-                        releases.add("r1" + sb + ".fastdebug.zero");
-                        releases.add("r2" + sb + ".fastdebug.zero");
-                        releases.add("r1" + sb + ".release.openj9");
-                        releases.add("r2" + sb + ".release.openj9");
-                    }
-                }
-                for (String r : releases) {
-                    File dd = new File(d, r);
-                    dd.mkdir();
-                    File dataDir = new File(dd, "data");
-                    dataDir.mkdir();
-                    Files.write(new File(dataDir, "info.log").toPath(), Arrays.asList(new String[]{product, v, r, dd.getAbsolutePath()}), StandardCharsets.UTF_8);
-                    for (String arch : archEnum) {
-                        File archDir = new File(dd, arch);
-                        archDir.mkdir();
-                        Files.write(new File(archDir, product + "-" + v + "-" + r + "." + arch + ".tarxz").toPath(), Arrays.asList(new String[]{product, v, r, arch, archDir.getAbsolutePath()}), StandardCharsets.UTF_8);
-                    }
-                }
-            }
+    }
+
+    @Test
+    public void getSourcesOfNotBuiltAtAll() {
+        final int expectedNumberOfBuilds = 4;
+        final Set<String> expectedArchives = new HashSet<>(Arrays.asList(
+                "java-1.8.0-openjdk-version1-release1.uName.src" + SUFFIX,
+                "java-1.8.0-openjdk-version1-release2.uName.src" + SUFFIX,
+                "java-1.8.0-openjdk-version2-release1.uName.src" + SUFFIX,
+                "java-1.8.0-openjdk-version2-release2.uName.src" + SUFFIX
+
+        ));
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=slowdebug buildPlatform=f29.x86_64",
+                "src",
+                false
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
+        for (final Build build : buildList) {
+            Assert.assertEquals(1, build.getRpms().size());
+            final RPM rpm = build.getRpms().get(0);
+            Assert.assertTrue(expectedArchives.contains(rpm.getFilename(SUFFIX)));
         }
-        //make java-1.8.0-openjdk-v2-r2.aarch64.shenandoah.release as rpms
-        File d3 = new File(builds.getAbsolutePath() + "/java-1.8.0-openjdk/v2/r2.aarch64.shenandoah.release");
-        for (String arch : archEnum) {
-            File d3ArchDir = new File(d3, arch);
-            File d3ArchFileSrcc = new File(d3ArchDir, "java-1.8.0-openjdk-v2-r2.aarch64.shenandoah.release." + arch + ".tarxz");
-            File d3ArchFileDest = new File(d3ArchDir, "java-1.8.0-openjdk-v2-r2.aarch64.shenandoah.release." + arch + ".rpm");
-            if (!d3ArchFileSrcc.renameTo(d3ArchFileDest)) {
-                throw new RuntimeException("cant rename test item 3");
-            }
+    }
+
+    @Test
+    public void getArchiveOfBuilt() {
+        final int expectedNumberOfBuilds = 4;
+        final Set<String> expectedArchives = new HashSet<>(Arrays.asList(
+                "java-1.8.0-openjdk-version1-release1.uName.release.hotspot.f29.x86_64" + SUFFIX,
+                "java-1.8.0-openjdk-version1-release2.uName.release.hotspot.f29.x86_64" + SUFFIX,
+                "java-1.8.0-openjdk-version2-release1.uName.release.hotspot.f29.x86_64" + SUFFIX,
+                "java-1.8.0-openjdk-version2-release2.uName.release.hotspot.f29.x86_64" + SUFFIX
+
+        ));
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=release",
+                "f29.x86_64",
+                true
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
+        for (final Build build : buildList) {
+            Assert.assertEquals(1, build.getRpms().size());
+            final RPM rpm = build.getRpms().get(0);
+            Assert.assertTrue(expectedArchives.contains(rpm.getFilename(SUFFIX)));
         }
-        //make java-1.8.0-openjdk-v1-r1.aarch64.shenandoah.release.zero as notyet built
-        File d2 = new File(builds.getAbsolutePath() + "/java-1.8.0-openjdk/v1/r1.aarch64.shenandoah.release.zero");
-        File d2ArchDir = new File(d2, "el7_x86_64");
-        File d2ArchFile = new File(d2ArchDir, "java-1.8.0-openjdk-v1-r1.aarch64.shenandoah.release.zero.el7_x86_64.tarxz");
-        if (!d2ArchFile.delete()) {
-            throw new RuntimeException("cant remove test item 2");
+    }
+
+    @Test
+    public void getArchiveOfPartiallyNotBuilt() {
+        final int expectedNumberOfBuilds = 2;
+        final Set<String> expectedArchives = new HashSet<>(Arrays.asList(
+                "java-1.8.0-openjdk-version1-release1.uName.fastdebug.hotspot.f29.x86_64" + SUFFIX,
+                "java-1.8.0-openjdk-version1-release2.uName.fastdebug.hotspot.f29.x86_64" + SUFFIX
+
+        ));
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=fastdebug",
+                "f29.x86_64",
+                true
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
+        for (final Build build : buildList) {
+            Assert.assertEquals(1, build.getRpms().size());
+            final RPM rpm = build.getRpms().get(0);
+            Assert.assertTrue(expectedArchives.contains(rpm.getFilename(SUFFIX)));
         }
-        //make java-11-openjdk-v2-r2.shenandoah.slowdebug failed
-        File d1 = new File(builds.getAbsolutePath() + "/java-11-openjdk/v2/r2.shenandoah.slowdebug/");
-        File d1ArchDir = new File(d1, "el7_x86_64");
-        File d1ArchFile = new File(d1ArchDir, "java-11-openjdk-v2-r2.shenandoah.slowdebug.el7_x86_64.tarxz");
-        if (!d1ArchFile.delete()) {
-            throw new RuntimeException("cant remove test item1");
-        }
-        File d1failFile = new File(d1ArchDir, "FAILED");
-        d1failFile.createNewFile();
-        as = new AccessibleSettings(builds, repos, null, null, null, null, 9848, 9849, 9822, 8080, 0);
-        kojiDB = new FakeKojiDB(as);
-    }
-
-    @AfterClass
-    public static void rmDb() {
-        dbRoot.deleteOnExit();
     }
 
     @Test
-    public void slowdebugHotspot() throws IOException, InterruptedException {
-        GetBuildList gbl = new GetBuildList("java-11-openjdk-shenandoah", "jvm=hotspot debugMode=slowdebug", true);
-        final GetBuildList getBuildListParams = GetBuildList.create(gbl);//??
-        //kojiDB.getArchives( )
-        List<Build> r = kojiDB.getBuildList(getBuildListParams);
-        //there is 8 mathcing records, however we removed one to be failed
-        //shouldbe missing java-11-openjdk-v2-r2.shenandoah.slowdebug
-        Assert.assertEquals(archEnum.length-1, r.size());
+    public void getArchiveOfNotBuiltAtAll() {
+        final int expectedNumberOfBuilds = 0;
+        final GetBuildList params = new GetBuildList(
+                DataGenerator.PROJECT_NAME_U,
+                "jvm=hotspot debugMode=slowdebug",
+                "f29.x86_64",
+                false
+        );
+        List<Build> buildList = kojiDB.getBuildList(params);
+        Assert.assertEquals(
+                getBuildNumberMessage(expectedNumberOfBuilds, buildList.size()),
+                expectedNumberOfBuilds,
+                buildList.size()
+        );
     }
 
-    @Test
-    public void releaseZero() throws IOException, InterruptedException {
-        GetBuildList gbl = new GetBuildList("java-1.8.0-openjdk-aarch64-shenandoah", "jvm=zero debugMode=release", true);
-        final GetBuildList getBuildListParams = GetBuildList.create(gbl);//??
-        //kojiDB.getArchives( )
-        List<Build> r = kojiDB.getBuildList(getBuildListParams);
-        //java-1.8.0-openjdk-v1-r1.aarch64.shenandoah.release.zero
-        //is forced to be not yet build in setup
-        Assert.assertEquals(archEnum.length-1, r.size());
-    }
-
-    @Test
-    public void releaseZeroToBeBUild() throws IOException, InterruptedException {
-        GetBuildList gbl = new GetBuildList("java-1.8.0-openjdk-aarch64-shenandoah", "jvm=zero debugMode=release", false);
-        final GetBuildList getBuildListParams = GetBuildList.create(gbl);//??
-        //kojiDB.getArchives( )
-        List<Build> r = kojiDB.getBuildList(getBuildListParams);
-        //java-1.8.0-openjdk-v1-r1.aarch64.shenandoah.release.zero
-        //is forced to be not yet build in setup
-        Assert.assertEquals(1, r.size());
-    }
-
-
-    @Test
-    public void oneGoneToOldApi() throws IOException, InterruptedException {
-        GetBuildList gbl = new GetBuildList("java-1.8.0-openjdk-aarch64-shenandoah", "jvm=hotspot debugMode=release", true);
-        final GetBuildList getBuildListParams = GetBuildList.create(gbl);//??
-        //kojiDB.getArchives( )
-        List<Build> r = kojiDB.getBuildList(getBuildListParams);
-        //there is 16 release of aarch64-shenandoah forjjdk8 hotspot (release and nothing are doublig it)
-        //hoewer java-1.8.0-openjdk-v2-r2.aarch64.shenandoah.release are rpms, thuis old api only
-        Assert.assertEquals((archEnum.length*2)-1, r.size());
-    }
-
-    @Test
-    public void oneForOldapi() throws IOException, InterruptedException {
-        GetBuildList gbl = new GetBuildList("java-1.8.0-openjdk-aarch64-shenandoah", "jvm=hotspot debugMode=release", true);
-        List<Build> r = kojiDB.getProjectBuilds(kojiDB.getPkgId("java-1.8.0-openjdk"), new HashSet<>());
-        //hoewer java-1.8.0-openjdk-v2-r2.aarch64.shenandoah.release are rpms, thuis old api only
-        Assert.assertEquals(1, r.size());
-    }
-
-    @Test
-    public void getBuildDetailsBasicTest() throws IOException, InterruptedException {
-        //usual one
-        GetBuildDetail requestedNvr = new GetBuildDetail("java-12-openjdk", "v1", "r1.updates");
-        Build b = kojiDB.getBuildDetail(requestedNvr);
-        Assert.assertEquals(archEnum.length, b.getRpms().size());
-        //the old api one
-        requestedNvr = new GetBuildDetail("java-1.8.0-openjdk", "v2", "r2.aarch64.shenandoah.release");
-        b = kojiDB.getBuildDetail(requestedNvr);
-        Assert.assertEquals(archEnum.length, b.getRpms().size());
-        //one normal from set with one dedicated to old api
-        requestedNvr = new GetBuildDetail("java-1.8.0-openjdk", "v1", "r1.aarch64.shenandoah.release");
-        b = kojiDB.getBuildDetail(requestedNvr);
-        Assert.assertEquals(archEnum.length, b.getRpms().size());
-        //one to be built
-        requestedNvr = new GetBuildDetail("java-1.8.0-openjdk", "v1", "r1.aarch64.shenandoah.release.zero");
-        b = kojiDB.getBuildDetail(requestedNvr);
-        Assert.assertEquals(archEnum.length-1/*last non built arch*/, b.getRpms().size());
-        // the failed el7_x86_64
-        requestedNvr = new GetBuildDetail("java-11-openjdk", "v2", "r2.shenandoah.slowdebug");
-        b = kojiDB.getBuildDetail(requestedNvr);
-        Assert.assertEquals(archEnum.length/*also failed build is listed, but file name is broken*/, b.getRpms().size());
+    private String getBuildNumberMessage(final int expected, final int actual) {
+        return "Expected number of builds: " + expected + ", got: " + actual;
     }
 }
---
+++ jenkins-scm-koji-plugin/src/test/java/hudson/plugins/scm/koji/client/OToolJenkinsTest.java
@@ -0,0 +1,177 @@
+package hudson.plugins.scm.koji.client;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+import hudson.plugins.scm.koji.FakeKojiXmlRpcApi;
+import hudson.plugins.scm.koji.KojiBuildProvider;
+import hudson.plugins.scm.koji.KojiSCM;
+import hudson.tasks.Shell;
+import org.fakekoji.core.FakeKojiTestUtil;
+import org.fakekoji.server.JavaServer;
+import org.fakekoji.xmlrpc.server.JavaServerConstants;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Collections;
+
+import static org.fakekoji.DataGenerator.JDK_8_PACKAGE_NAME;
+import static org.fakekoji.DataGenerator.PROJECT_NAME_U;
+import static org.fakekoji.DataGenerator.RELEASE_1;
+import static org.fakekoji.DataGenerator.SOURCES;
+import static org.fakekoji.DataGenerator.SUFFIX;
+import static org.fakekoji.DataGenerator.VERSION_1;
+import static org.junit.Assert.assertEquals;
+
+public class OToolJenkinsTest {
+
+    @ClassRule
+    public static TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    private static JavaServer javaServer = null;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        File tmpDir = temporaryFolder.newFolder();
+        javaServer = FakeKojiTestUtil.createDefaultFakeKojiServerWithData(tmpDir);
+        javaServer.start();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        if (javaServer != null) {
+            javaServer.stop();
+        }
+    }
+
+    @Test
+    public void testBuildJobFailure() throws Exception {
+        /* Test koji scm plugin on existing fake-koji build(s)
+           -> should end with success */
+        final String expectedFile = JDK_8_PACKAGE_NAME + '-' + VERSION_1 + '-' + RELEASE_1 + '.' + PROJECT_NAME_U + '.' + SOURCES + SUFFIX;
+        String shellString = "find . | grep \"" + expectedFile + "\"";
+        runTest(
+                new FakeKojiXmlRpcApi(
+                        PROJECT_NAME_U,
+                        "debugMode=release jvm=hotspot buildPlatform=f29.x86_64",
+                        "src",
+                        false
+                ),
+                shellString,
+                false
+        );
+    }
+
+    @Test
+    public void testBuildJobSuccess() throws Exception {
+        final String expectedFile = "java-1.8.0-openjdk-version2-release2.uName.src.tarxz";
+        String shellString = "find . | grep \"" + expectedFile + "\"\n";
+        runTest(
+                new FakeKojiXmlRpcApi(
+                        PROJECT_NAME_U,
+                        "debugMode=fastdebug jvm=hotspot buildPlatform=f29.x86_64",
+                        "src",
+                        false
+                ),
+                shellString,
+                true
+        );
+    }
+
+    @Test
+    public void testTestJobFailure() throws Exception {
+        final String expectedFile = "java-1.8.0-openjdk-version1-release2.uName.slowdebug.hotspot.f29.x86_64.tarxz";
+        String shellString = "find . | grep \"" + expectedFile + "\"\n";
+        runTest(
+                new FakeKojiXmlRpcApi(
+                        PROJECT_NAME_U,
+                        "debugMode=slowdebug jvm=hotspot",
+                        "f29.x86_64",
+                        true
+                ),
+                shellString,
+                false
+        );
+    }
+
+    @Test
+    public void testTestJobSuccess() throws Exception {
+        final String expectedFile = "java-1.8.0-openjdk-version1-release2.uName.fastdebug.hotspot.f29.x86_64.tarxz";
+        String shellString = "find . | grep \"" + expectedFile + "\"\n";
+        runTest(
+                new FakeKojiXmlRpcApi(
+                        PROJECT_NAME_U,
+                        "debugMode=fastdebug jvm=hotspot",
+                        "f29.x86_64",
+                        true
+                ),
+                shellString,
+                true
+        );
+    }
+
+    @Test
+    public void testTestJobWithSourcesSuccess() throws Exception {
+        final String expectedFile = "java-1.8.0-openjdk-version1-release2.uName.fastdebug.hotspot.f29.x86_64.tarxz";
+        final String expectedSourceFile = "java-1.8.0-openjdk-version1-release2.uName.src.tarxz";
+        String shellString = "find . | grep \"" + expectedFile + "\"\nfind . | grep \"" + expectedSourceFile + "\"\n";
+        runTest(
+                new FakeKojiXmlRpcApi(
+                        PROJECT_NAME_U,
+                        "debugMode=fastdebug jvm=hotspot",
+                        "f29.x86_64 src",
+                        true
+                ),
+                shellString,
+                true
+        );
+    }
+
+    private void runTest(FakeKojiXmlRpcApi kojiXmlRpcApi, String shellScript, boolean successExpected) throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        KojiSCM scm = new KojiSCM(
+                Collections.singletonList(createLocalhostBuildProvider()),
+                kojiXmlRpcApi,
+                null,
+                false,
+                false,
+                10
+        );
+        /* set new KojiSCM plugin instance as scm for project */
+        project.setScm(scm);
+        /* set shell string executed by the project */
+        project.getBuildersList().add(new Shell(shellScript));
+        /* schedule build and get it (wait for it to finish) */
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        /* Print log (of the build) to stdout */
+        try (FileInputStream fis = new FileInputStream(build.getLogFile())) {
+            for (;;) {
+                int readByte = fis.read();
+                if (readByte < 0) {
+                    break;
+                }
+                System.out.write(readByte);
+            }
+        }
+        /* get result of the build and check it it meets expectations */
+        Result result = build.getResult();
+        assertEquals(successExpected, result == Result.SUCCESS);
+    }
+
+    private KojiBuildProvider createLocalhostBuildProvider() {
+        return new KojiBuildProvider(
+                "http://localhost:" + JavaServerConstants.xPortAxiom + "/RPC2",
+                "http://localhost:" + JavaServerConstants.dPortAxiom
+        );
+    }
+}
