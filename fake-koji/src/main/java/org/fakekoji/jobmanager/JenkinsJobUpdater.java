5a7c55787418d0867550ea874c453148ff5d9187
oklinov <oklinovs@redhat.com>
2019-11-07 13:48:06 +0100

    JobUdater returns JobUpdateResults

--- fake-koji/src/main/java/org/fakekoji/jobmanager/JenkinsJobUpdater.java
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/JenkinsJobUpdater.java
@@ -2,6 +2,8 @@ package org.fakekoji.jobmanager;
 
 import org.fakekoji.Utils;
 import org.fakekoji.jobmanager.model.Job;
+import org.fakekoji.jobmanager.model.JobUpdateResult;
+import org.fakekoji.jobmanager.model.JobUpdateResults;
 import org.fakekoji.xmlrpc.server.JavaServerConstants;
 
 import java.io.File;
@@ -9,8 +11,12 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.logging.Logger;
 
 public class JenkinsJobUpdater implements JobUpdater {
@@ -27,16 +33,28 @@ public class JenkinsJobUpdater implements JobUpdater {
     }
 
     @Override
-    public void update(Set<Job> oldJobs, Set<Job> newJobs) throws IOException {
-        final Set<String> archivedJobs = new HashSet<>(Arrays.asList(jobArchiveRoot.list()));
+    public JobUpdateResults update(Set<Job> oldJobs, Set<Job> newJobs) {
+
+        final Function<Job, JobUpdateResult> rewriteFunction = jobUpdateFunctionWrapper(getRewriteFunction());
+        final Function<Job, JobUpdateResult> createFunction = jobUpdateFunctionWrapper(getCreateFunction());
+        final Function<Job, JobUpdateResult> archiveFunction = jobUpdateFunctionWrapper(getArchiveFunction());
+        final Function<Job, JobUpdateResult> reviveFunction = jobUpdateFunctionWrapper(getReviveFunction());
+
+        final List<JobUpdateResult> jobsCreated = new LinkedList<>();
+        final List<JobUpdateResult> jobsArchived = new LinkedList<>();
+        final List<JobUpdateResult> jobsRewritten = new LinkedList<>();
+        final List<JobUpdateResult> jobsRevived= new LinkedList<>();
+
+        final Set<String> archivedJobs = new HashSet<>(Arrays.asList(Objects.requireNonNull(jobArchiveRoot.list())));
+
         for (final Job job : oldJobs) {
             if (newJobs.stream().noneMatch(newJob -> job.toString().equals(newJob.toString()))) {
-                archive(job);
+                jobsArchived.add(archiveFunction.apply(job));
             }
         }
         for (final Job job : newJobs) {
             if (archivedJobs.contains(job.toString())) {
-                revive(job);
+                jobsRevived.add(reviveFunction.apply(job));
                 continue;
             }
             final Optional<Job> optional = oldJobs.stream()
@@ -45,54 +63,87 @@ public class JenkinsJobUpdater implements JobUpdater {
             if (optional.isPresent()) {
                 final Job oldJob = optional.get();
                 if (!oldJob.equals(job)) {
-                    update(job);
+                    jobsRewritten.add(rewriteFunction.apply(job));
                 }
                 continue;
             }
-            create(job);
+            jobsCreated.add(createFunction.apply(job));
         }
-    }
-
-    private void create(Job job) throws IOException {
-        final String jobName = job.toString();
-        LOGGER.info("Creating job " + jobName);
-        final String jobsRootPath = jobsRoot.getAbsolutePath();
-        LOGGER.info("Creating directory " + jobName + " in " + jobsRootPath);
-        final File jobDir = Paths.get(jobsRootPath, jobName).toFile();
-        if (!jobDir.mkdir()) {
-            throw new IOException("Could't create file: " + jobDir.getAbsolutePath());
-        }
-        final String jobDirPath = jobDir.getAbsolutePath();
-        LOGGER.info("Creating file " + JENKINS_JOB_CONFIG_FILE + " in " + jobDirPath);
-        Utils.writeToFile(
-                Paths.get(jobDirPath, JENKINS_JOB_CONFIG_FILE),
-                job.generateTemplate()
+        return new JobUpdateResults(
+                jobsCreated,
+                jobsArchived,
+                jobsRewritten,
+                jobsRevived
         );
     }
 
-    private void revive(Job job) throws IOException {
-        final String jobName = job.toString();
-        final File src = Paths.get(jobArchiveRoot.getAbsolutePath(), job.toString()).toFile();
-        final File dst = Paths.get(jobsRoot.getAbsolutePath(), job.toString()).toFile();
-        LOGGER.info("Reviving job " + jobName);
-        LOGGER.info("Moving directory " + src.getAbsolutePath() + " to " + dst.getAbsolutePath());
-        Utils.moveFile(src, dst);
+    private Function<Job, JobUpdateResult> jobUpdateFunctionWrapper(JobUpdateFunction updateFunction) {
+        return job -> {
+            try {
+                return updateFunction.apply(job);
+            } catch (IOException e) {
+                LOGGER.warning(e.getMessage());
+                return new JobUpdateResult(job.toString(), false, e.getMessage());
+            }
+        };
     }
 
-    private void archive(Job job) throws IOException {
-        final String jobName = job.toString();
-        final File src = Paths.get(jobsRoot.getAbsolutePath(), job.toString()).toFile();
-        final File dst = Paths.get(jobArchiveRoot.getAbsolutePath(), job.toString()).toFile();
-        LOGGER.info("Archiving job " + jobName);
-        LOGGER.info("Moving directory " + src.getAbsolutePath() + " to " + dst.getAbsolutePath());
-        Utils.moveFile(src, dst);
+    private JobUpdateFunction getCreateFunction() {
+        return job -> {
+            final String jobName = job.toString();
+            LOGGER.info("Creating job " + jobName);
+            final String jobsRootPath = jobsRoot.getAbsolutePath();
+            LOGGER.info("Creating directory " + jobName + " in " + jobsRootPath);
+            final File jobDir = Paths.get(jobsRootPath, jobName).toFile();
+            if (!jobDir.mkdir()) {
+                throw new IOException("Could't create file: " + jobDir.getAbsolutePath());
+            }
+            final String jobDirPath = jobDir.getAbsolutePath();
+            LOGGER.info("Creating file " + JENKINS_JOB_CONFIG_FILE + " in " + jobDirPath);
+            Utils.writeToFile(
+                    Paths.get(jobDirPath, JENKINS_JOB_CONFIG_FILE),
+                    job.generateTemplate()
+            );
+            return new JobUpdateResult(jobName, true);
+        };
     }
 
-    private void update(Job job) throws IOException {
-        final String jobName = job.toString();
-        final File jobConfig = Paths.get(jobsRoot.getAbsolutePath(), jobName, JENKINS_JOB_CONFIG_FILE).toFile();
-        LOGGER.info("Updating job " + jobName);
-        LOGGER.info("Writing to file " + jobConfig.getAbsolutePath());
-        Utils.writeToFile(jobConfig, job.generateTemplate());
+    private JobUpdateFunction getReviveFunction() {
+        return job -> {
+            final String jobName = job.toString();
+            final File src = Paths.get(jobArchiveRoot.getAbsolutePath(), job.toString()).toFile();
+            final File dst = Paths.get(jobsRoot.getAbsolutePath(), job.toString()).toFile();
+            LOGGER.info("Reviving job " + jobName);
+            LOGGER.info("Moving directory " + src.getAbsolutePath() + " to " + dst.getAbsolutePath());
+            Utils.moveFile(src, dst);
+            return new JobUpdateResult(jobName, true);
+        };
+    }
+
+    private JobUpdateFunction getArchiveFunction() {
+        return job -> {
+            final String jobName = job.toString();
+            final File src = Paths.get(jobsRoot.getAbsolutePath(), job.toString()).toFile();
+            final File dst = Paths.get(jobArchiveRoot.getAbsolutePath(), job.toString()).toFile();
+            LOGGER.info("Archiving job " + jobName);
+            LOGGER.info("Moving directory " + src.getAbsolutePath() + " to " + dst.getAbsolutePath());
+            Utils.moveFile(src, dst);
+            return new JobUpdateResult(jobName, true);
+        };
+    }
+
+    private JobUpdateFunction getRewriteFunction() {
+        return job -> {
+            final String jobName = job.toString();
+            final File jobConfig = Paths.get(jobsRoot.getAbsolutePath(), jobName, JENKINS_JOB_CONFIG_FILE).toFile();
+            LOGGER.info("Rewriting job " + jobName);
+            LOGGER.info("Writing to file " + jobConfig.getAbsolutePath());
+            Utils.writeToFile(jobConfig, job.generateTemplate());
+            return new JobUpdateResult(jobName, true);
+        };
+    }
+
+    interface JobUpdateFunction {
+        JobUpdateResult apply(Job job) throws IOException;
     }
 }
--- fake-koji/src/main/java/org/fakekoji/jobmanager/JobUpdater.java
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/JobUpdater.java
@@ -1,11 +1,12 @@
 package org.fakekoji.jobmanager;
 
 import org.fakekoji.jobmanager.model.Job;
+import org.fakekoji.jobmanager.model.JobUpdateResults;
 
 import java.io.IOException;
 import java.util.Set;
 
 public interface JobUpdater {
 
-    void update(Set<Job> oldJobs, Set<Job> newJobs) throws IOException;
+    JobUpdateResults update(Set<Job> oldJobs, Set<Job> newJobs);
 }
