fec4ea4cd2badf1306e996c2c1dee1f4beda3843
oklinov <oklinovs@redhat.com>
2019-11-13 16:54:10 +0100

    Rewrite JobConfig component

--- webapp/src/components/formComponents/JobConfigComponent.tsx
+++ webapp/src/components/formComponents/JobConfigComponent.tsx
@@ -1,65 +1,198 @@
-import React from "react";
-import { JobConfig, PlatformConfig } from "../../stores/model";
-import PlatformComponent from "./PlatformComponent";
-import AddComponent from "./AddComponent";
-import { ConfigStore, CONFIG_STORE } from "../../stores/ConfigStore";
-import { inject, observer } from "mobx-react";
-import TreeNode from "../TreeNode";
+import React from "react"
+import { JobConfig, PlatformConfig, VariantsConfig, TaskConfig, TaskType } from "../../stores/model"
+import AddComponent from "./AddComponent"
+import { ConfigStore, CONFIG_STORE } from "../../stores/ConfigStore"
+import { inject, observer } from "mobx-react"
+import { Table, TableHead, TableRow, TableCell, TableBody, Tooltip, IconButton } from "@material-ui/core"
+import Select from "./Select"
+import { Add, Delete } from "@material-ui/icons"
 
 interface Props {
-    jobConfig: JobConfig;
-    configStore?: ConfigStore;
+    jobConfig: JobConfig
+    configStore?: ConfigStore
 }
 
 class JobConfigComponent extends React.PureComponent<Props> {
 
     onPlatformAdd = (id: string, platformConfig: PlatformConfig = { tasks: {} }): void => {
-        this.props.jobConfig.platforms[id] = platformConfig;
+        this.props.jobConfig.platforms[id] = platformConfig
     }
 
     onPlatformDelete = (id: string): void => {
-        delete this.props.jobConfig.platforms[id];
+        delete this.props.jobConfig.platforms[id]
+    }
+
+    platformRows = (key: string, platformConfigs: { [id: string]: PlatformConfig }, type: TaskType): JSX.Element[] => {
+        const configStore = this.props.configStore!
+        return Object.keys(platformConfigs).flatMap(id => {
+            const platformConfig = platformConfigs[id]
+            const taskConfigs = platformConfig.tasks
+
+            const unselectedTasks = configStore.tasks
+                .filter(task => task.type === type && !taskConfigs[task.id])
+            const cell: JSX.Element = (
+                <span>
+                    {id}
+                    <AddComponent
+                        label={`Add ${type.toLowerCase()} task`}
+                        items={unselectedTasks}
+                        onAdd={(taskId) => { platformConfig.tasks[taskId] = { variants: [] } }} />
+                </span>
+            )
+
+            return [
+                <TableRow key={key + id}>
+                    <TableCell>{type === "BUILD" && cell}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>{type === "TEST" && cell}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                        <IconButton onClick={() => { delete platformConfigs[id] }}>
+                            <Delete />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>,
+                ...this.taskRows(key + id, taskConfigs, type)
+            ]
+        })
+    }
+
+    taskRows = (key: string, taskConfigs: { [id: string]: TaskConfig }, type: TaskType): JSX.Element[] => {
+        return Object.keys(taskConfigs).flatMap(id => {
+            const taskConfig = taskConfigs[id]
+            const taskVariants = taskConfig.variants
+
+            const cell: JSX.Element = (
+                <span>
+                    {id}
+                    <Tooltip title={`Add ${type.toLowerCase()} variant`}>
+                        <IconButton onClick={() => { taskVariants.splice(0, 0, { map: {}, platforms: (type === "BUILD" && {}) || undefined }) }}>
+                            <Add />
+                        </IconButton>
+                    </Tooltip>
+                </span>
+            )
+
+            return [
+                <TableRow key={key + id}>
+                    <TableCell></TableCell>
+                    <TableCell>{type === "BUILD" && cell}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>{type === "TEST" && cell}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>
+                        <IconButton onClick={() => { delete taskConfigs[id] }}>
+                            <Delete />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>,
+                ...this.taskVariantRows(key + id, taskVariants, type)
+            ]
+        })
+    }
+
+    taskVariantRows = (key: string, variantConfigs: VariantsConfig[], type: TaskType): JSX.Element[] => {
+        const configStore = this.props.configStore!
+        const testVariantRows: JSX.Element[] = variantConfigs.flatMap((variant, index) => {
+            const taskVariants = configStore!.taskVariants.filter(variant => variant.type === type)
+
+            const unselectedPlatforms = variant.platforms && type === "BUILD" && configStore.platforms
+                .filter(platform => !variant.platforms![platform.id])
+
+            const cell = (
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    {
+                        taskVariants.map(taskVariant =>
+                            <Select
+                                options={Object.values(taskVariant.variants).map(variant => variant.id)}
+                                label={taskVariant.id}
+                                value={variant.map[taskVariant.id]}
+                                onChange={(value: string) => { variant.map[taskVariant.id] = value }}
+                                key={taskVariant.id} />
+                        )
+                    }
+                    {
+                        unselectedPlatforms && <AddComponent
+                            label={`Add test platform`}
+                            items={unselectedPlatforms}
+                            onAdd={(taskId) => {
+                                if (!variant.platforms) {
+                                    variant.platforms = {}
+                                }
+                                variant.platforms[taskId] = { tasks: {} }
+                            }} />
+                    }
+                </div>
+            )
+
+            const platformRows = (type === "BUILD" && variant.platforms && this.platformRows(key + index, variant.platforms, "TEST")) || []
+
+            const testVariantRow: JSX.Element[] = [
+                <TableRow key={key + index}>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>{type === "BUILD" && cell}</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>{type === "TEST" && cell}</TableCell>
+                    <TableCell>
+                        <IconButton onClick={() => { variantConfigs.splice(index, 1) }}>
+                            <Delete />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>,
+                ...platformRows
+            ]
+
+            return testVariantRow
+        })
+        return testVariantRows
     }
 
     render() {
-        const { configStore, jobConfig } = this.props;
-        const platformConfigs = jobConfig.platforms;
-        const platforms = configStore!.platforms;
-        const platformConfigIds = Object.keys(platformConfigs);
-        const unselectedPlatforms = platforms.filter(platform => !platformConfigIds.includes(platform.id));
+
+        const { configStore, jobConfig } = this.props
+        const buildPlatformConfigs = jobConfig.platforms
+
+        const unselectedPlatforms = configStore!.platforms
+            .filter(platform => !buildPlatformConfigs[platform.id])
+
         return (
             <div>
-                <TreeNode>
-                    <TreeNode.Title>
-                        Job Configuration
-                    </TreeNode.Title>
-                    <TreeNode.NodeInfo>
-                        Platforms ({platformConfigIds.length})
-                    </TreeNode.NodeInfo>
-                    <TreeNode.Options>
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                <span>
+                                    build platform
+                                    <AddComponent
+                                        label={`Add build platform`}
+                                        items={unselectedPlatforms}
+                                        onAdd={(platformId) => {
+                                            buildPlatformConfigs[platformId] = { tasks: {} }
+                                        }} />
+                                </span>
+                            </TableCell>
+                            <TableCell>build task</TableCell>
+                            <TableCell>build variants</TableCell>
+                            <TableCell>test platform</TableCell>
+                            <TableCell>test task</TableCell>
+                            <TableCell>test variants</TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                         {
-                            unselectedPlatforms.length !== 0 && <AddComponent
-                                onAdd={this.onPlatformAdd}
-                                items={unselectedPlatforms}
-                                label={"Add platform"} />
+                            this.platformRows("job", buildPlatformConfigs, "BUILD")
                         }
-                    </TreeNode.Options>
-                    <TreeNode.ChildNodes>
-                        {
-                            platformConfigIds.map(id =>
-                                <PlatformComponent
-                                    onDelete={this.onPlatformDelete}
-                                    key={id}
-                                    id={id}
-                                    config={platformConfigs[id]}
-                                    type={"BUILD"} />
-                            )
-                        }
-                    </TreeNode.ChildNodes>
-                </TreeNode>
+                    </TableBody>
+                </Table>
             </div>
         )
     }
 }
 
-export default inject(CONFIG_STORE)(observer(JobConfigComponent));
+export default inject(CONFIG_STORE)(observer(JobConfigComponent))
