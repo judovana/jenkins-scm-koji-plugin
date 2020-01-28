2ecaa3740f329b11d53cc5fad49d25c314111880
Jiri Vanek <jvanek@redhat.com>
2020-01-28 10:34:51 +0100

    Added dummy class with main to be used from container

---
+++ fake-koji/src/main/java/org/fakekoji/core/DummyMain.java
@@ -0,0 +1,9 @@
+package org.fakekoji.core;
+
+//Please do not delete this, untill we make yarn to work in contianer
+
+public class DummyMain {
+    public static void main(String[] args) {
+        System.out.println("Fake koji is on classapth");
+    }
+}
