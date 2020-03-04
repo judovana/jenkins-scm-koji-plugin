ba07893423efb99f8aab53efacaa391b0511270b
oklinov <oklinovs@redhat.com>
2020-03-04 13:52:56 +0100

    Set focus on 'yes' button and set onClose callback in ConfirmDialog

--- webapp/src/components/ConfirmDialog.tsx
+++ webapp/src/components/ConfirmDialog.tsx
@@ -8,6 +8,18 @@ interface ConfirmDialogProps {}
 
 const ConfirmDialog: React.FC<ConfirmDialogProps> = () => {
     const { viewStore } = useStores()
+    const yesButton = React.useRef<HTMLButtonElement | null>(null)
+
+    const onEnter = React.useCallback(() => {
+        if (!viewStore.dialog.open) {
+            return
+        }
+        if (!yesButton.current) {
+            return
+        }
+        yesButton.current.focus()
+    }, [viewStore.dialog.open])
+
     return useObserver(() => {
         const { closeDialog, dialog } = viewStore
         const onYesClick = () => {
@@ -18,10 +30,12 @@ const ConfirmDialog: React.FC<ConfirmDialogProps> = () => {
             closeDialog()
         }
         return (
-            <Dialog open={dialog.open}>
+            <Dialog onEnter={onEnter} open={dialog.open} onClose={closeDialog}>
                 <DialogTitle>Are you sure?</DialogTitle>
                 <DialogActions>
-                    <Button onClick={onYesClick}>yes</Button>
+                    <Button onClick={onYesClick} ref={yesButton}>
+                        yes
+                    </Button>
                     <Button onClick={onNoClick}>cancel</Button>
                 </DialogActions>
             </Dialog>
