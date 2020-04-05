78ed2e0957c68ffd5dfebbce67d076d8066e0107
oklinov <oklinovs@redhat.com>
2020-04-05 17:51:39 +0200

    Set basis for bumping jobs and implement PlatformBumper

--- fake-koji/src/main/java/org/fakekoji/api/http/rest/OToolService.java
+++ fake-koji/src/main/java/org/fakekoji/api/http/rest/OToolService.java
@@ -15,6 +15,7 @@ import org.fakekoji.jobmanager.model.JDKProject;
 import org.fakekoji.jobmanager.model.JDKTestProject;
 import org.fakekoji.jobmanager.model.JobUpdateResults;
 import org.fakekoji.jobmanager.project.JDKProjectManager;
+import org.fakekoji.jobmanager.project.JDKProjectParser;
 import org.fakekoji.jobmanager.project.JDKTestProjectManager;
 import org.fakekoji.model.Platform;
 import org.fakekoji.model.Task;
@@ -47,7 +48,7 @@ public class OToolService {
     private static final String BUILD_PROVIDER = BUILD_PROVIDERS + CONFIG_ID;
     private static final String JDK_VERSIONS = "/jdkVersions";
     private static final String JDK_VERSION = JDK_VERSIONS + CONFIG_ID;
-    private static final String PLATFORMS = "/platforms";
+    static final String PLATFORMS = "/platforms";
     private static final String PLATFORM = PLATFORMS + CONFIG_ID;
     private static final String TASKS = "/tasks";
     private static final String TASK = TASKS + CONFIG_ID;
@@ -58,7 +59,7 @@ public class OToolService {
     private static final String JDK_TEST_PROJECTS = "/jdkTestProjects";
     private static final String JDK_TEST_PROJECT = JDK_TEST_PROJECTS + CONFIG_ID;
     private static final String GET = "get";
-
+    private static final String BUMP = "bump";
     private static final String MISC = "misc";
     private static final String HELP = "help";
     static final String FILTER = "filter";
@@ -460,6 +461,18 @@ public class OToolService {
                     taskVariantManager,
                     platformManager,
                     taskManager
+            ));
+            final JDKProjectParser parser = new JDKProjectParser(
+                    ConfigManager.create(settings.getConfigRoot().getAbsolutePath()),
+                    settings.getLocalReposRoot(),
+                    settings.getScriptsRoot()
+            );
+            path(BUMP, new BumperAPI(
+                    jenkinsJobUpdater,
+                    parser,
+                    jdkProjectManager,
+                    jdkTestProjectManager,
+                    platformManager
             ));
         });
     }
---
+++ fake-koji/src/main/java/org/fakekoji/api/http/rest/RestUtils.java
@@ -0,0 +1,18 @@
+package org.fakekoji.api.http.rest;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+class RestUtils {
+
+    static Optional<String> extractParamValue(Map<String, List<String>> paramsMap, String param) {
+        return Optional.ofNullable(paramsMap.get(param))
+                .filter(list -> list.size() == 1)
+                .map(list -> list.get(0));
+    }
+
+    static Optional<List<String>> extractParamValues(Map<String, List<String>> paramsMap, String param) {
+        return Optional.ofNullable(paramsMap.get(param));
+    }
+}
