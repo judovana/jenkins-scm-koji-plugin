70d8e05f1f3598bf9d1687dbea64027a3975ec53
oklinov <oklinovs@redhat.com>
2020-04-05 18:02:05 +0200

    Update tests

--- fake-koji/src/test/java/org/fakekoji/jobmanager/project/JDKProjectParserTest.java
+++ fake-koji/src/test/java/org/fakekoji/jobmanager/project/JDKProjectParserTest.java
@@ -1,6 +1,7 @@
 package org.fakekoji.jobmanager.project;
 
 import org.fakekoji.DataGenerator;
+import org.fakekoji.functional.Result;
 import org.fakekoji.jobmanager.ConfigManager;
 import org.fakekoji.jobmanager.ManagementException;
 import org.fakekoji.jobmanager.model.BuildJob;
@@ -12,6 +13,7 @@ import org.fakekoji.jobmanager.model.PullJob;
 import org.fakekoji.jobmanager.model.TestJob;
 import org.fakekoji.model.BuildProvider;
 import org.fakekoji.model.JDKVersion;
+import org.fakekoji.model.OToolVariable;
 import org.fakekoji.model.Platform;
 import org.fakekoji.model.Task;
 import org.fakekoji.model.TaskVariant;
@@ -34,6 +36,7 @@ import java.util.Set;
 
 import static org.fakekoji.DataGenerator.BEAKER;
 import static org.fakekoji.DataGenerator.PROJECT_NAME;
+import static org.fakekoji.DataGenerator.PROJECT_URL;
 import static org.fakekoji.DataGenerator.TEST_PROJECT_NAME;
 import static org.fakekoji.DataGenerator.VAGRANT;
 
@@ -46,11 +49,14 @@ public class JDKProjectParserTest {
     private File repositoriesRoot;
     private File configsRoot;
     private File scriptsRoot;
+    private Set<TestJob> testOnlyJobs;
+    private Set<Job> jdkProjectJobs;
 
     private final JDKTestProject jdkTestProject = DataGenerator.getJDKTestProject();
     private final List<String> blacklist = jdkTestProject.getSubpackageBlacklist();
     private final List<String> whitelist = jdkTestProject.getSubpackageWhitelist();
     private final JDKVersion jdkVersion = DataGenerator.getJDKVersion8();
+    private final Task buildTask = DataGenerator.getBuildTask();
     private final Task tckTask = DataGenerator.getTCK();
     private final Task jtregTask = DataGenerator.getJTREG();
     private final Platform rhel7x64 = DataGenerator.getRHEL7x64();
@@ -65,11 +71,8 @@ public class JDKProjectParserTest {
         scriptsRoot = temporaryFolder.newFolder("scripts");
         DataGenerator.initConfigsRoot(configsRoot);
         configManager = ConfigManager.create(configsRoot.getAbsolutePath());
-    }
 
-    @Test
-    public void parseJDKTestProject() throws StorageException, ManagementException {
-        final Set<Job> expectedJobs = new HashSet<>(Arrays.asList(
+        testOnlyJobs = new HashSet<>(Arrays.asList(
                 new TestJob(
                         VAGRANT,
                         TEST_PROJECT_NAME,
@@ -86,6 +89,8 @@ public class JDKProjectParserTest {
                                 }}
                         ),
                         rhel7x64,
+                        null,
+                        new Task(),
                         Collections.unmodifiableMap(
                                 new HashMap<TaskVariant, TaskVariantValue>() {{
                                     put(DataGenerator.getDebugModeVariant(), DataGenerator.getSlowdebugVariant());
@@ -112,6 +117,8 @@ public class JDKProjectParserTest {
                                 }}
                         ),
                         rhel7x64,
+                        null,
+                        new Task(),
                         Collections.unmodifiableMap(
                                 new HashMap<TaskVariant, TaskVariantValue>() {{
                                     put(DataGenerator.getDebugModeVariant(), DataGenerator.getSlowdebugVariant());
@@ -138,6 +145,8 @@ public class JDKProjectParserTest {
                                 }}
                         ),
                         rhel7x64,
+                        null,
+                        new Task(),
                         Collections.unmodifiableMap(
                                 new HashMap<TaskVariant, TaskVariantValue>() {{
                                     put(DataGenerator.getDebugModeVariant(), DataGenerator.getSlowdebugVariant());
@@ -164,6 +173,8 @@ public class JDKProjectParserTest {
                                 }}
                         ),
                         rhel7x64,
+                        null,
+                        new Task(),
                         Collections.unmodifiableMap(
                                 new HashMap<TaskVariant, TaskVariantValue>() {{
                                     put(DataGenerator.getDebugModeVariant(), DataGenerator.getSlowdebugVariant());
@@ -190,6 +201,8 @@ public class JDKProjectParserTest {
                                 }}
                         ),
                         rhel7x64,
+                        null,
+                        new Task(),
                         Collections.unmodifiableMap(
                                 new HashMap<TaskVariant, TaskVariantValue>() {{
                                     put(DataGenerator.getDebugModeVariant(), DataGenerator.getSlowdebugVariant());
@@ -202,122 +215,124 @@ public class JDKProjectParserTest {
                 )
         ));
 
+        final PullJob pullJob = new PullJob(
+                PROJECT_NAME,
+                PROJECT_URL,
+                DataGenerator.getJDK8Product(),
+                jdkVersion,
+                repositoriesRoot,
+                scriptsRoot,
+                Collections.emptyList()
+        );
+        final BuildJob buildJobHotspotRelease = new BuildJob(
+                BEAKER,
+                PROJECT_NAME,
+                DataGenerator.getJDK8Product(),
+                jdkVersion,
+                buildProviders,
+                buildTask,
+                rhel7x64,
+                Collections.unmodifiableMap(
+                        new HashMap<TaskVariant, TaskVariantValue>() {{
+                            put(DataGenerator.getJvmVariant(), DataGenerator.getHotspotVariant());
+                            put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
+                        }}
+                ),
+                scriptsRoot,
+                Collections.emptyList()
+        );
+        final BuildJob buildJobZeroRelease = new BuildJob(
+                BEAKER,
+                PROJECT_NAME,
+                DataGenerator.getJDK8Product(),
+                jdkVersion,
+                buildProviders,
+                buildTask,
+                rhel7x64,
+                Collections.unmodifiableMap(
+                        new HashMap<TaskVariant, TaskVariantValue>() {{
+                            put(DataGenerator.getJvmVariant(), DataGenerator.getZeroVariant());
+                            put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
+                        }}
+                ),
+                scriptsRoot,
+                Collections.emptyList()
+        );
+        final TestJob testJobHotspotRelease = new TestJob(
+                VAGRANT,
+                buildJobHotspotRelease,
+                tckTask,
+                rhel7x64,
+                Collections.unmodifiableMap(
+                        new HashMap<TaskVariant, TaskVariantValue>() {{
+                            put(DataGenerator.getGarbageCollectorCategory(), DataGenerator.getShenandoahVariant());
+                            put(DataGenerator.getDisplayProtocolCategory(), DataGenerator.getXServerVariant());
+                        }}
+                )
+        );
+        final TestJob testJobZeroHotspot = new TestJob(
+                BEAKER,
+                buildJobZeroRelease,
+                tckTask,
+                rhel7x64,
+                Collections.unmodifiableMap(
+                        new HashMap<TaskVariant, TaskVariantValue>() {{
+                            put(DataGenerator.getGarbageCollectorCategory(), DataGenerator.getShenandoahVariant());
+                            put(DataGenerator.getDisplayProtocolCategory(), DataGenerator.getXServerVariant());
+                        }}
+                )
+        );
+
+        jdkProjectJobs = new HashSet<>(Arrays.asList(
+                pullJob,
+                buildJobHotspotRelease,
+                buildJobZeroRelease,
+                testJobHotspotRelease,
+                testJobZeroHotspot
+        ));
+    }
+
+    @Test
+    public void parseJDKTestProject() throws StorageException, ManagementException {
         final JDKProjectParser parser = new JDKProjectParser(configManager, repositoriesRoot, scriptsRoot);
         final Set<Job> actualJobs = parser.parse(jdkTestProject);
         Assert.assertEquals(
                 "ParsedProject should be equal",
-                expectedJobs,
+                testOnlyJobs,
                 actualJobs
+        );
+    }
+
+    @Test
+    public void parseJDKTestProjectJobs() {
+        final JDKProjectParser parser = new JDKProjectParser(configManager, repositoriesRoot, scriptsRoot);
+        final Result<JDKTestProject, String> result = parser.parseJDKTestProjectJobs(testOnlyJobs);
+        Assert.assertEquals(
+                DataGenerator.getJDKTestProject(),
+                result.getValue()
         );
     }
 
     @Test
     public void parseJDKProject() throws StorageException, ManagementException {
         final JDKProject jdkProject = DataGenerator.getJDKProject();
-        final Task buildTask = DataGenerator.getBuildTask();
-        final Set<BuildProvider> buildProviders = DataGenerator.getBuildProviders();
-
-        final Set<Job> expectedJobs = new HashSet<>(Arrays.asList(
-                new PullJob(
-                        PROJECT_NAME,
-                        DataGenerator.getJDK8Product(),
-                        jdkVersion,
-                        repositoriesRoot,
-                        scriptsRoot,
-                        Collections.emptyList()
-                ),
-                new BuildJob(
-                        BEAKER,
-                        PROJECT_NAME,
-                        DataGenerator.getJDK8Product(),
-                        jdkVersion,
-                        buildProviders,
-                        buildTask,
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getJvmVariant(), DataGenerator.getHotspotVariant());
-                                    put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
-                                }}
-                        ),
-                        scriptsRoot,
-                        Collections.emptyList()
-                ),
-                new BuildJob(
-                        BEAKER,
-                        PROJECT_NAME,
-                        DataGenerator.getJDK8Product(),
-                        jdkVersion,
-                        buildProviders,
-                        buildTask,
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getJvmVariant(), DataGenerator.getZeroVariant());
-                                    put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
-                                }}
-                        ),
-                        scriptsRoot,
-                        Collections.emptyList()
-                ),
-                new TestJob(
-                        VAGRANT,
-                        PROJECT_NAME,
-                        Project.ProjectType.JDK_PROJECT,
-                        DataGenerator.getJDK8Product(),
-                        jdkVersion,
-                        buildProviders,
-                        tckTask,
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getGarbageCollectorCategory(), DataGenerator.getShenandoahVariant());
-                                    put(DataGenerator.getDisplayProtocolCategory(), DataGenerator.getXServerVariant());
-                                }}
-                        ),
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getJvmVariant(), DataGenerator.getHotspotVariant());
-                                    put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
-                                }}
-                        ),
-                        scriptsRoot,
-                        Collections.emptyList()
-                ),
-                new TestJob(
-                        BEAKER,
-                        PROJECT_NAME,
-                        Project.ProjectType.JDK_PROJECT,
-                        DataGenerator.getJDK8Product(),
-                        jdkVersion,
-                        buildProviders,
-                        tckTask,
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getGarbageCollectorCategory(), DataGenerator.getShenandoahVariant());
-                                    put(DataGenerator.getDisplayProtocolCategory(), DataGenerator.getXServerVariant());
-                                }}
-                        ),
-                        rhel7x64,
-                        Collections.unmodifiableMap(
-                                new HashMap<TaskVariant, TaskVariantValue>() {{
-                                    put(DataGenerator.getJvmVariant(), DataGenerator.getZeroVariant());
-                                    put(DataGenerator.getDebugModeVariant(), DataGenerator.getReleaseVariant());
-                                }}
-                        ),
-                        scriptsRoot,
-                        Collections.emptyList()
-                )
-        ));
 
         final JDKProjectParser parser = new JDKProjectParser(configManager, repositoriesRoot, scriptsRoot);
         final Set<Job> actualJobs = parser.parse(jdkProject);
         Assert.assertEquals(
                 "ParsedProject should be equal",
-                expectedJobs,
+                jdkProjectJobs,
                 actualJobs
+        );
+    }
+
+    @Test
+    public void parseJDKProjectJobs() {
+        final JDKProjectParser parser = new JDKProjectParser(configManager, repositoriesRoot, scriptsRoot);
+        final Result<JDKProject, String> result = parser.parseJDKProjectJobs(jdkProjectJobs);
+        Assert.assertEquals(
+                DataGenerator.getJDKProject(),
+                result.getValue()
         );
     }
 }
