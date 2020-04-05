b1aee6929d202dda2f01715c9690817aac662db5
oklinov <oklinovs@redhat.com>
2020-04-05 17:28:09 +0200

    Refactor OToolService and ConfigManagers (jobs will be updated by service, not by managers)

--- fake-koji/src/main/java/org/fakekoji/core/utils/matrix/MatrixGenerator.java
+++ fake-koji/src/main/java/org/fakekoji/core/utils/matrix/MatrixGenerator.java
@@ -2,7 +2,6 @@ package org.fakekoji.core.utils.matrix;
 
 import org.fakekoji.core.AccessibleSettings;
 import org.fakekoji.jobmanager.ConfigManager;
-import org.fakekoji.jobmanager.JenkinsJobUpdater;
 import org.fakekoji.jobmanager.ManagementException;
 import org.fakekoji.jobmanager.manager.*;
 import org.fakekoji.jobmanager.model.*;
@@ -57,18 +56,16 @@ public class MatrixGenerator {
     public MatrixGenerator(AccessibleSettings settings, ConfigManager configManager, String testRegex, String buildRegex, TestEqualityFilter testEqualityFilter,
                            BuildEqualityFilter buildEqualityFilter, String[] project) {
 
-        final JenkinsJobUpdater jenkinsJobUpdater = new JenkinsJobUpdater(settings);
-
         this.testFilter = testEqualityFilter;
         this.buildFilter = buildEqualityFilter;
         this.testRegex = Pattern.compile(testRegex == null ? defaultRegex : testRegex);
         this.buildRgex = Pattern.compile(buildRegex == null ? defaultRegex : buildRegex);
         buildProviderManager = new BuildProviderManager(configManager.getBuildProviderStorage());
-        platformManager = new PlatformManager(configManager.getPlatformStorage(), jenkinsJobUpdater);
+        platformManager = new PlatformManager(configManager.getPlatformStorage());
         //contains both BUILD and TEST variants
         taskVariantManager = new TaskVariantManager(configManager.getTaskVariantStorage());
         jDKVersionManager = new JDKVersionManager(configManager.getJdkVersionStorage());
-        taskManager = new TaskManager(configManager.getTaskStorage(), jenkinsJobUpdater);
+        taskManager = new TaskManager(configManager.getTaskStorage());
 
         this.project = project;
         if (project != null) {
@@ -76,12 +73,10 @@ public class MatrixGenerator {
         }
 
         jdkTestProjectManager = new JDKTestProjectManager(
-                configManager.getJdkTestProjectStorage(),
-                jenkinsJobUpdater
+                configManager.getJdkTestProjectStorage()
         );
         jdkProjectManager = new JDKProjectManager(
                 configManager,
-                jenkinsJobUpdater,
                 settings.getLocalReposRoot(),
                 settings.getScriptsRoot()
         );
--- fake-koji/src/main/java/org/fakekoji/jobmanager/Manager.java
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/Manager.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public interface Manager<T> {
 
-    ManagementResult create(T t) throws StorageException, ManagementException;
+    T create(T t) throws StorageException, ManagementException;
 
     T read(String id) throws StorageException, ManagementException;
 
     List<T> readAll() throws StorageException;
 
-    ManagementResult update(String id, T t) throws StorageException, ManagementException;
+    T update(String id, T t) throws StorageException, ManagementException;
 
-    ManagementResult delete(String id) throws StorageException, ManagementException;
+    T delete(String id) throws StorageException, ManagementException;
 
     boolean contains(String id);
 }
