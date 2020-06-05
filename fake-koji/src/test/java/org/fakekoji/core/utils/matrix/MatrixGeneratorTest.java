d698a28958919c41eed6223a200c8065bf31359a
oklinov <oklinovs@redhat.com>
2020-06-05 15:08:49 +0200

    Create matrix tests

--- fake-koji/src/test/java/org/fakekoji/core/utils/matrix/MatrixGeneratorTest.java
+++ fake-koji/src/test/java/org/fakekoji/core/utils/matrix/MatrixGeneratorTest.java
@@ -10,12 +10,19 @@ import org.fakekoji.jobmanager.model.JDKProject;
 import org.fakekoji.jobmanager.model.JDKTestProject;
 import org.fakekoji.jobmanager.model.Project;
 import org.fakekoji.storage.StorageException;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,18 +41,25 @@ public class MatrixGeneratorTest {
     }
 
     @Test
-    public void showFullMatrix() throws ManagementException, StorageException {
+    public void showFullMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
         MatrixGenerator m = new MatrixGenerator(configManager, new String[0]);
         List<BuildSpec> bs = m.getBuilds();
         List<TestSpec> ts = m.getTests();
-        m.printMatrix(System.out, bs, ts, false, false, tf);
-        m.printMatrix(System.out, ts, bs, true, true, tf);
+
+        final File matrixFile = temporaryFolder.newFile();
+        final String expectedContent = readResource("full_dropped_matrix");
+        m.printMatrix(new PrintStream(matrixFile), ts, bs, true, true, tf);
+        assertContents(expectedContent, matrixFile);
+
     }
 
     @Test
-    public void showFullSqeezedMatrix() throws ManagementException, StorageException {
+    public void showFullSqueezedMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
+        final File matrixFile = temporaryFolder.newFile();
+        final String expectedContent = readResource("squeezed_matrix");
+        final PrintStream printStream = new PrintStream(matrixFile);
         MatrixGenerator m = new MatrixGenerator(
                 configManager,
                 ".*",
@@ -56,36 +70,65 @@ public class MatrixGeneratorTest {
         );
         List<BuildSpec> bs = m.getBuilds();
         List<TestSpec> ts = m.getTests();
-        m.printMatrix(System.out, bs, ts, true, true, tf);
-        m.printMatrix(System.out, ts, bs, false, false, tf);
+        m.printMatrix(printStream, bs, ts, true, true, tf);
+        m.printMatrix(printStream, ts, bs, false, false, tf);
+        assertContents(expectedContent, matrixFile);
     }
 
     @Test
-    public void showPerProjectMatrix() throws ManagementException, StorageException {
+    public void showPerProjectMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
         final ConfigCache configCache = new ConfigCache(configManager);
+        final File matrixFile = temporaryFolder.newFile();
+        final String expectedContent = readResource("per_project_matrices");
+        final PrintStream printStream = new PrintStream(matrixFile);
         for (Project project : configCache.getProjects()) {
             MatrixGenerator m = new MatrixGenerator(configManager, new String[]{project.getId()});
             List<BuildSpec> bs = m.getBuilds();
             List<TestSpec> ts = m.getTests();
-            m.printMatrix(System.out, ts, bs, true, true, tf);
+            m.printMatrix(printStream, ts, bs, true, true, tf);
         }
+        assertContents(expectedContent, matrixFile);
     }
 
     @Test
-    public void showPerProjectsMatrix() throws ManagementException, StorageException {
+    public void showPerProjectsMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
         final ConfigCache configCache = new ConfigCache(configManager);
         final List<String> jdkProjectNames = configCache.getJdkProjects().stream().map(JDKProject::getId).collect(Collectors.toList());
         final List<String> jdkTestProjectNames = configCache.getJdkTestProjects().stream().map(JDKTestProject::getId).collect(Collectors.toList());
         final List<List<String>> jdkProjectLists = Arrays.asList(jdkProjectNames, jdkTestProjectNames);
+        final File matrixFile = temporaryFolder.newFile();
+        final String expectedContent = readResource("per_projects_matrices");
+        final PrintStream printStream = new PrintStream(matrixFile);
         for (List<String> projectNames : jdkProjectLists) {
             MatrixGenerator m = new MatrixGenerator(configManager, projectNames.toArray(new String[0]));
             List<BuildSpec> bs = m.getBuilds();
             List<TestSpec> ts = m.getTests();
-            m.printMatrix(System.out, ts, bs, true, true, tf);
+            m.printMatrix(printStream, ts, bs, true, true, tf);
         }
+        assertContents(expectedContent, matrixFile);
+    }
 
+    private void assertContents(final String content1, final File matrixFile) throws IOException {
+        Assert.assertEquals(content1, readFileTrimLines(matrixFile));
+    }
 
+    private String readFileTrimLines(final InputStream inputStream) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        final StringBuilder content = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            content.append(line.trim()).append('\n');
+        }
+        return content.toString();
+    }
+
+    private String readFileTrimLines(final File file) throws IOException {
+        return readFileTrimLines(new FileInputStream(file));
+    }
+
+    private String readResource(final String name) throws IOException {
+        return readFileTrimLines(this.getClass().getResourceAsStream("/org/fakekoji/core/utils/matrix/" + name));
     }
 }
