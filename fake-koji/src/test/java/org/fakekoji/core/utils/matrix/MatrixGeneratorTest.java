4dd9ed3adaea0045984281b0cd053ce5eba064b7
oklinov <oklinovs@redhat.com>
2020-06-05 18:27:00 +0200

    Use Utils and ByteArrayOutputStream instead of File in matrix tests

--- fake-koji/src/test/java/org/fakekoji/core/utils/matrix/MatrixGeneratorTest.java
+++ fake-koji/src/test/java/org/fakekoji/core/utils/matrix/MatrixGeneratorTest.java
@@ -2,10 +2,11 @@ package org.fakekoji.core.utils.matrix;
 
 
 import org.fakekoji.DataGenerator;
+import org.fakekoji.Utils;
 import org.fakekoji.core.AccessibleSettings;
 import org.fakekoji.jobmanager.ConfigCache;
-import org.fakekoji.jobmanager.ManagementException;
 import org.fakekoji.jobmanager.ConfigManager;
+import org.fakekoji.jobmanager.ManagementException;
 import org.fakekoji.jobmanager.model.JDKProject;
 import org.fakekoji.jobmanager.model.JDKTestProject;
 import org.fakekoji.jobmanager.model.Project;
@@ -16,11 +17,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Arrays;
@@ -46,20 +45,19 @@ public class MatrixGeneratorTest {
         MatrixGenerator m = new MatrixGenerator(configManager, new String[0]);
         List<BuildSpec> bs = m.getBuilds();
         List<TestSpec> ts = m.getTests();
-
-        final File matrixFile = temporaryFolder.newFile();
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         final String expectedContent = readResource("full_dropped_matrix");
-        m.printMatrix(new PrintStream(matrixFile), ts, bs, true, true, tf);
-        assertContents(expectedContent, matrixFile);
+        m.printMatrix(new PrintStream(outputStream), ts, bs, true, true, tf);
+        assertContents(expectedContent, outputStream);
 
     }
 
     @Test
     public void showFullSqueezedMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
-        final File matrixFile = temporaryFolder.newFile();
         final String expectedContent = readResource("squeezed_matrix");
-        final PrintStream printStream = new PrintStream(matrixFile);
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final PrintStream printStream = new PrintStream(outputStream);
         MatrixGenerator m = new MatrixGenerator(
                 configManager,
                 ".*",
@@ -72,23 +70,23 @@ public class MatrixGeneratorTest {
         List<TestSpec> ts = m.getTests();
         m.printMatrix(printStream, bs, ts, true, true, tf);
         m.printMatrix(printStream, ts, bs, false, false, tf);
-        assertContents(expectedContent, matrixFile);
+        assertContents(expectedContent, outputStream);
     }
 
     @Test
     public void showPerProjectMatrix() throws ManagementException, StorageException, IOException {
         final ConfigManager configManager = settings.getConfigManager();
         final ConfigCache configCache = new ConfigCache(configManager);
-        final File matrixFile = temporaryFolder.newFile();
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         final String expectedContent = readResource("per_project_matrices");
-        final PrintStream printStream = new PrintStream(matrixFile);
+        final PrintStream printStream = new PrintStream(outputStream);
         for (Project project : configCache.getProjects()) {
             MatrixGenerator m = new MatrixGenerator(configManager, new String[]{project.getId()});
             List<BuildSpec> bs = m.getBuilds();
             List<TestSpec> ts = m.getTests();
             m.printMatrix(printStream, ts, bs, true, true, tf);
         }
-        assertContents(expectedContent, matrixFile);
+        assertContents(expectedContent, outputStream);
     }
 
     @Test
@@ -98,37 +96,30 @@ public class MatrixGeneratorTest {
         final List<String> jdkProjectNames = configCache.getJdkProjects().stream().map(JDKProject::getId).collect(Collectors.toList());
         final List<String> jdkTestProjectNames = configCache.getJdkTestProjects().stream().map(JDKTestProject::getId).collect(Collectors.toList());
         final List<List<String>> jdkProjectLists = Arrays.asList(jdkProjectNames, jdkTestProjectNames);
-        final File matrixFile = temporaryFolder.newFile();
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         final String expectedContent = readResource("per_projects_matrices");
-        final PrintStream printStream = new PrintStream(matrixFile);
+        final PrintStream printStream = new PrintStream(outputStream);
         for (List<String> projectNames : jdkProjectLists) {
             MatrixGenerator m = new MatrixGenerator(configManager, projectNames.toArray(new String[0]));
             List<BuildSpec> bs = m.getBuilds();
             List<TestSpec> ts = m.getTests();
             m.printMatrix(printStream, ts, bs, true, true, tf);
         }
-        assertContents(expectedContent, matrixFile);
+        assertContents(expectedContent, outputStream);
     }
 
-    private void assertContents(final String content1, final File matrixFile) throws IOException {
-        Assert.assertEquals(content1, readFileTrimLines(matrixFile));
-    }
-
-    private String readFileTrimLines(final InputStream inputStream) throws IOException {
-        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        final StringBuilder content = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            content.append(line.trim()).append('\n');
-        }
-        return content.toString();
-    }
-
-    private String readFileTrimLines(final File file) throws IOException {
-        return readFileTrimLines(new FileInputStream(file));
+    private void assertContents(final String expected, final ByteArrayOutputStream outputStream) throws IOException {
+        final String actual = String.join("\n", Utils.readStreamToLines(
+                new InputStreamReader(new ByteArrayInputStream(outputStream.toByteArray())),
+                String::trim
+        ));
+        Assert.assertEquals(expected, actual);
     }
 
     private String readResource(final String name) throws IOException {
-        return readFileTrimLines(this.getClass().getResourceAsStream("/org/fakekoji/core/utils/matrix/" + name));
+        return String.join("\n", Utils.readStreamToLines(
+                new InputStreamReader(this.getClass().getResourceAsStream("/org/fakekoji/core/utils/matrix/" + name)),
+                String::trim
+        ));
     }
 }
