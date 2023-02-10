233109072f2b344ef21930fec78435f2c2930bbf
Jiri Vanek <jvanek@redhat.com>
2023-02-10 11:10:04 +0100

    proeprly distingusih build and test variants

--- fake-koji/src/main/java/org/fakekoji/jobmanager/bumpers/impl/VariantBumper.java
+++ fake-koji/src/main/java/org/fakekoji/jobmanager/bumpers/impl/VariantBumper.java
@@ -4,6 +4,7 @@ import org.fakekoji.core.AccessibleSettings;
 import org.fakekoji.jobmanager.bumpers.JobModifier;
 import org.fakekoji.jobmanager.model.BuildJob;
 import org.fakekoji.jobmanager.model.TestJob;
+import org.fakekoji.model.Task;
 import org.fakekoji.model.TaskVariant;
 import org.fakekoji.model.TaskVariantValue;
 
@@ -19,6 +20,7 @@ public class VariantBumper extends JobModifier {
     private final String to;
     private final Pattern filter;
     private final List<TaskVariant> variants;
+    private final TaskVariant sharedParent;
 
 
     public VariantBumper(
@@ -36,18 +38,25 @@ public class VariantBumper extends JobModifier {
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
+
+        TaskVriantWithValue fromVariant = getVariantByValue(from);
+        TaskVriantWithValue toVariant = getVariantByValue(to);
+        if (!fromVariant.tv.equals(toVariant.tv)) {
+            throw new RuntimeException("Variants must be from same group, are not - " + from + "/" + to + " " + fromVariant.tv.getId() + "/" + toVariant.tv.getId());
+        }
+        sharedParent = fromVariant.tv;
     }
 
     @Override
     protected boolean shouldPass(BuildJob job) {
-        return job.getVariants().values().stream().map(a->a.getId()).collect(Collectors.toList()).contains(from) && matches(job.getName());
+        return job.getVariants().values().stream().map(a -> a.getId()).collect(Collectors.toList()).contains(from) && matches(job.getName());
     }
 
     @Override
     protected boolean shouldPass(TestJob job) {
         return (
-                job.getVariants().values().stream().map(a->a.getId()).collect(Collectors.toList()).contains(from)
-                        || job.getBuildVariants().values().stream().map(a->a.getId()).collect(Collectors.toList()).contains(from)
+                job.getVariants().values().stream().map(a -> a.getId()).collect(Collectors.toList()).contains(from)
+                        || job.getBuildVariants().values().stream().map(a -> a.getId()).collect(Collectors.toList()).contains(from)
         ) && matches(job.getName());
     }
 
@@ -65,7 +74,7 @@ public class VariantBumper extends JobModifier {
                 job.getBuildProviders(),
                 job.getTask(),
                 job.getPlatform(),
-                modifyVariant(job.getVariants(), from, to),
+                (sharedParent.getType().equals(Task.Type.BUILD))?modifyVariant(job.getVariants(), from, to):job.getVariants(),
                 job.getScriptsRoot(),
                 job.getProjectVariables()
         );
@@ -82,11 +91,11 @@ public class VariantBumper extends JobModifier {
                 job.getBuildProviders(),
                 job.getTask(),
                 job.getPlatform(),
-                modifyVariant(job.getVariants(), from, to),
+                (sharedParent.getType().equals(Task.Type.TEST))?modifyVariant(job.getVariants(), from, to):job.getVariants(),
                 job.getBuildPlatform(),
                 job.getBuildPlatformProvider(),
                 job.getBuildTask(),
-                modifyVariant(job.getBuildVariants(), from, to),
+                (sharedParent.getType().equals(Task.Type.BUILD))?modifyVariant(job.getBuildVariants(), from, to):job.getBuildVariants(),
                 job.getProjectSubpackageBlacklist(),
                 job.getProjectSubpackageWhitelist(),
                 job.getScriptsRoot(),
@@ -98,7 +107,7 @@ public class VariantBumper extends JobModifier {
         Map<TaskVariant, TaskVariantValue> result = new HashMap<>(origs.size());
         for (Map.Entry<TaskVariant, TaskVariantValue> orig : origs.entrySet()) {
             if (orig.getValue().getId().equals(from)) {
-                result.put(orig.getKey(), getVariantByValue(to));
+                result.put(orig.getKey(), getVariantByValue(to).tvv);
             } else {
                 result.put(orig.getKey(), orig.getValue());
             }
@@ -106,13 +115,23 @@ public class VariantBumper extends JobModifier {
         return result;
     }
 
-    private TaskVariantValue getVariantByValue(String value) {
+    private static class TaskVriantWithValue {
+        private final TaskVariant tv;
+        private final TaskVariantValue tvv;
+
+        public TaskVriantWithValue(TaskVariant tv, TaskVariantValue tvv) {
+            this.tv = tv;
+            this.tvv = tvv;
+        }
+    }
+
+    private TaskVriantWithValue getVariantByValue(String value) {
         int i = 0;
         for (TaskVariant tv : variants) {
             for (TaskVariantValue tvv : tv.getVariants().values()) {
                 i++;
                 if (tvv.getId().equals(value)) {
-                    return tvv;
+                    return new TaskVriantWithValue(tv, tvv);
                 }
             }
         }
